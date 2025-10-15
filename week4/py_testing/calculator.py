132daed8869331bf754f09c54237281c66132e86
CalleFreme <cj.freme@gmail.com>
2025-10-15 11:54:09 +0200

    Remove experimentation code

--- week4/py_testing/calculator.py
+++ week4/py_testing/calculator.py
@@ -10,11 +10,3 @@ def division(a, b):
         return a / b # Vanlig division
     except ZeroDivisionError:
         raise ValueError
-    
-while True:
-    num1 = int(input("Enter first number: "))
-    num2 = int(input("Enter second number: "))
-    try:
-        print(f"{num1} + {num2} = {add(num1, num2)}")
-    except ValueError:
-        print("Invalid input, please enter numbers only.")
